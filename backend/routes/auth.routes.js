9819c1cbdd68a0334ddf7b1b8a9da183d8c77803
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-07-23 19:47:44 +0800

    route middleware added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,8 +2,16 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/signup", (req, res) => {
+    res.send("signup Route");
+});
+
 router.get("/login", (req, res) => {
     res.send("Login Route");
+});
+
+router.get("/logout", (req, res) => {
+    res.send("Logout Route");
 });
 
 export default router;
