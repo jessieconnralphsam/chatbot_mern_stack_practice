15f64230e9567b7a73666b72680fab0032c81083
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-07-24 05:45:48 +0800

    postman worked  [201]

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,45 @@
-export const signup = async(req, res) => {
-    try{
-        const {fullName, username, password, confirmPassword, gender} = req.body;
-    }catch(error){
+import User from '../models/user.model.js';
 
+export const signup = async (req, res) => {
+    try {
+        const { fullName, username, password, confirmPassword, gender } = req.body;
+        // console.log("Request body: ", req.body);
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Mali imong password choiii" });
+        }
+
+        const user = await User.findOne({ username });
+        if (user) {
+            return res.status(400).json({ error: "Naa na ang username" });
+        }
+
+        //hash password here
+        //https://avatar-placeholder.iran.liara.run/
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullname: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic
+        });
+
+    } catch (error) {
+        console.error("Error sa signup controller", error.message);
+        res.status(500).json({ error: "Internal Error parekoy!" });
     }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
-
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    //root route http://localhost:5000/
-    res.send("Hello world!!")
-});
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//     root route http://localhost:5000/
+//     res.send("Hello world!!")
+// });
 
 app.listen(PORT, () => {
     connectToMongoDB();
