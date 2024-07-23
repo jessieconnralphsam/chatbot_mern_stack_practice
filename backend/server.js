73c0d443ea4a611347f5f8c9fdb564f2c41e7b30
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-07-23 19:40:04 +0800

    [add login route]

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from "express";
-
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
-
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
@@ -12,16 +11,6 @@ app.get("/", (req, res) => {
     res.send("Hello world!!")
 });
 
-app.get("/api/auth/signup", (req, res) => {
-    console.log("signup route")
-});
-
-app.get("/api/auth/login", (req, res) => {
-    console.log("login route")
-});
-
-app.get("/api/auth/login", (req, res) => {
-    console.log("logout route")
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
